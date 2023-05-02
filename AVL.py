550288eeab3d0b1b3581e508684e48a1c061ff67
Steph <79061296+stephgrotewold@users.noreply.github.com>
2023-05-02 11:17:52 -0600

    Update AVL
    Borrar comentarios el inicio del archivo que no son parte del codigo.

--- AVL.py
+++ AVL.py
@@ -1,19 +1,5 @@
 #aqui pongamos los metodos de insert,search, delete,min,max y traverse.
 
-'''
-Se requiere hacer los siguientes cambios del codigo del BSL para que sea un AVL
-
-1) Cambiar la estructura de nodo a una estructura que contenga la altura del nodo y el factor de equilibrio.
-
-2) Añadir una función para calcular la altura de un nodo, que tomará en cuenta la altura de sus subárboles.
-
-3) Añadir una función para calcular el factor de equilibrio de un nodo, que se define como la diferencia de altura entre el subárbol derecho y el subárbol izquierdo.
-
-4) Añadir las rotaciones necesarias (rotación simple y doble a la derecha y a la izquierda) para mantener el árbol equilibrado después de la inserción y eliminación de nodos.
-
-5) Actualizar la altura y el factor de equilibrio de los nodos después de cada operación.
-'''
-
 from graphviz import Digraph
 from typing import List
 class Node:
